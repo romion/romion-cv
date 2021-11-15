06190f7948205de68f760715bef0751d20eb66a1
sazhnev <rsazhniev@gloriumtech.com>
2021-11-15 23:27:22 +0200

    add dist folder

---
+++ dist/scripts.js
@@ -0,0 +1,116 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scss/app.scss":
+/*!***************************!*\
+  !*** ./src/scss/app.scss ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://romion-cv/./src/scss/app.scss?");
+
+/***/ }),
+
+/***/ "./src/scss/typography.scss":
+/*!**********************************!*\
+  !*** ./src/scss/typography.scss ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://romion-cv/./src/scss/typography.scss?");
+
+/***/ }),
+
+/***/ "./src/index.html":
+/*!************************!*\
+  !*** ./src/index.html ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"<!doctype html>\\n<html lang=\\\"en\\\">\\n<head>\\n    <meta charset=\\\"UTF-8\\\">\\n    <meta name=\\\"viewport\\\"\\n          content=\\\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\\\">\\n    <meta http-equiv=\\\"X-UA-Compatible\\\" content=\\\"ie=edge\\\">\\n    <link rel='stylesheet' href='style.css'>\\n    <title>Roman Sazhnev CV</title>\\n</head>\\n<body>\\n    <div class=\\\"container\\\">\\n        <div class=\\\"header\\\">\\n            <img class=\\\"header__avatar\\\" src=\\\"./assets/images/ava.jpg\\\" alt=\\\"avatar\\\">\\n            <div class=\\\"header__profile-info\\\">\\n                <span class=\\\"json-info\\\"><span class=\\\"md-tag\\\">```</span> { age: <strong>27</strong>, email: \\\"<strong>r.sazhnev@gmail.com</strong>\\\", phone: \\\"<strong>+38-066-934-9338</strong>\\\", english: \\\"<strong>Upper-Intermediate</strong>\\\" } <span class=\\\"md-tag\\\">```</span></span>\\n                <h1><span class=\\\"md-tag\\\">#</span> Roman Sazhniev</h1>\\n                <h3><span class=\\\"md-tag\\\">//</span> JavaScript Developer</h3>\\n            </div>\\n        </div>\\n        <div class=\\\"content\\\">\\n            <div class=\\\"content-group skills\\\">\\n                <h2><span class=\\\"md-tag\\\">##</span> Tech Skills</h2>\\n                <div class=\\\"skills__group\\\">\\n                    <div class=\\\"skills__group__item\\\">\\n                        <h3><span class=\\\"md-tag\\\">1.</span> Main Skills</h3>\\n                        <ul>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Strong knowledge of modern <strong>JS</strong> / <strong>TS</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Strong knowledge of <strong>HTML</strong> / <strong>CSS</strong> + <strong>Preprocessors</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span><strong>Angular</strong> (~5 year exp.) / <strong>RxJs</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span><strong>React</strong> / <strong>Redux</strong> / <strong>Redux Toolkit</strong> / <strong>RTK Query</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Unit testing: <strong>Jasmine</strong> / <strong>Karma</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Task managers: <strong>Webpack</strong> / <strong>Gulp</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Mobile: <strong>Responsive markup</strong> / <strong>React Native</strong> / <strong>PWA</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>UI Libs: <strong>Material</strong> / <strong>PrimeNG</strong> / <strong>Ant Design</strong> etc.</li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Experience with <strong>Git</strong> and <strong>NPM</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Experience with <strong>SSR</strong></li>\\n                        </ul>\\n                    </div>\\n                    <div class=\\\"skills__group__item\\\">\\n                        <h3><span class=\\\"md-tag\\\">2.</span> Back-End skills</h3>\\n                        <ul>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span><strong>NodeJS</strong> / <strong>NestJS</strong> (ability to create well-documented secured API with JWT auth and user roles)</li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Databases: <strong>PostgreSQL</strong> / <strong>MySQL</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>API Documentation: <strong>Swagger</strong></li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Huge experience with <strong>Firebase</strong></li>\\n                        </ul>\\n                        <h3 class=\\\"margin-top\\\"><span class=\\\"md-tag\\\">3.</span> Other skills</h3>\\n                        <ul>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>Ability to configure server with database in Linux system using SSH, set up domain and SSL sertificate</li>\\n                            <li><span class=\\\"md-tag\\\">- [x]</span>UI/UX Skills: <strong>Photoshop</strong> / <strong>Figma</strong></li>\\n                        </ul>\\n                    </div>\\n                </div>\\n            </div>\\n            <div class=\\\"content-group skills margin-top-40\\\">\\n                <h2><span class=\\\"md-tag\\\">##</span> Soft Skills</h2>\\n                <div class=\\\"skills__group\\\">\\n                    <ul>\\n                        <li><span class=\\\"md-tag\\\">- [x]</span><strong>Teamwork</strong> skills: work experience both in small teams and in teams of 50+ people</li>\\n                        <li><span class=\\\"md-tag\\\">- [x]</span><strong>Communication</strong> skills: polite and productive communication with clients, management and team</li>\\n                        <li><span class=\\\"md-tag\\\">- [x]</span><strong>Interpersonal</strong> skills: ability to navigate social interactions with colleagues </li>\\n                        <li><span class=\\\"md-tag\\\">- [x]</span><strong>Analytical</strong> and <strong>Problem-solving</strong> skills: ability to analyze and evaluate processes and operations, estimate tasks and solve problems in project and team</li>\\n                        <li><span class=\\\"md-tag\\\">- [x]</span><strong>Mentoring</strong>: training new employees and helping teams implement improvement plans, approving pull-requests</li>\\n                    </ul>\\n                </div>\\n            </div>\\n            <div class=\\\"content-group exp margin-top-40\\\">\\n                <h2><span class=\\\"md-tag\\\">##</span> Experience</h2>\\n                <div class=\\\"exp__group\\\">\\n                    <div class=\\\"exp__name\\\">\\n                        <img src=\\\"assets/images/glorium.png\\\" alt=\\\"Glorium Technologies\\\">\\n                        <div class=\\\"exp__name__info\\\">\\n                            <h3 class=\\\"title\\\">Glorium Technologies</h3>\\n                            <span class=\\\"years\\\">September 2018 — today</span>\\n                        </div>\\n                    </div>\\n                    <div class=\\\"exp__projects\\\">\\n                        <div class=\\\"exp__projects__item\\\">\\n                            <h3><span class=\\\"md-tag\\\">- [ ]</span> Real Estate Project</h3>\\n                            <p>It's a fully integrated asset & portfolio management application that helps investors, portfolio, asset and property-managers to optimally manage their global investments.</p>\\n                            <ul>\\n                                <li>* <strong>Position</strong>: Front-End Developer</li>\\n                                <li>* <strong>Team</strong>: 50+ developers</li>\\n                                <li>* <strong>Technologies</strong>: Angular, RxJs, SCSS, AG Grid etc.</li>\\n                            </ul>\\n                        </div>\\n                        <div class=\\\"exp__projects__item\\\">\\n                            <h3><span class=\\\"md-tag\\\">- [ ]</span> Healthcare Project</h3>\\n                            <p>It's a solution for German clinics which provides a streamline order intake, handle scheduling and delivery processes, manage documentation flow, provides intuitive billing and claims processing.</p>\\n                            <ul>\\n                                <li>* <strong>Position</strong>: Front-End Developer</li>\\n                                <li>* <strong>Team</strong>: 30+ developers</li>\\n                                <li>* <strong>Technologies</strong>: Angular, RxJs, SCSS, Material etc.</li>\\n                            </ul>\\n                        </div>\\n                    </div>\\n                </div>\\n\\n                <div class=\\\"exp__group\\\">\\n                    <div class=\\\"exp__name\\\">\\n                        <img src=\\\"assets/images/jt-tech.jpeg\\\" alt=\\\"JS Technology Solutions\\\">\\n                        <div class=\\\"exp__name__info\\\">\\n                            <h3 class=\\\"title\\\">JS Technology Solutions</h3>\\n                            <span class=\\\"years\\\">March 2018 — September 2018</span>\\n                        </div>\\n                    </div>\\n                    <div class=\\\"exp__projects\\\">\\n                        <div class=\\\"exp__projects__item\\\">\\n                            <h3><span class=\\\"md-tag\\\">- [ ]</span> Survey Builder Project</h3>\\n                            <p>It's a complex survey builder engine with a lot of question types and custom libs of UI components. Work directly on American client company.</p>\\n                            <ul>\\n                                <li>* <strong>Position</strong>: Full-Stack Developer</li>\\n                                <li>* <strong>Team</strong>: 3 developers</li>\\n                                <li>* <strong>Technologies</strong>: React, Redux, SCSS, NodeJS, Express etc.</li>\\n                            </ul>\\n                        </div>\\n                    </div>\\n                </div>\\n\\n                <div class=\\\"exp__group\\\">\\n                    <div class=\\\"exp__name\\\">\\n                        <img src=\\\"assets/images/buddha.jpg\\\" alt=\\\"BuddhaSoft\\\">\\n                        <div class=\\\"exp__name__info\\\">\\n                            <h3 class=\\\"title\\\">BuddhaSoft</h3>\\n                            <span class=\\\"years\\\">November 2017 — March 2018</span>\\n                        </div>\\n                    </div>\\n                    <div class=\\\"exp__projects\\\">\\n                        <div class=\\\"exp__projects__item\\\">\\n                            <h3><span class=\\\"md-tag\\\">- [ ]</span> Healthcare Project</h3>\\n                            <p>Another healthcare application for a network of private Swedish clinics.</p>\\n                            <ul>\\n                                <li>* <strong>Position</strong>: Full-Stack Developer</li>\\n                                <li>* <strong>Team</strong>: 5 developers</li>\\n                                <li>* <strong>Technologies</strong>: Angular, RxJs, Stylus, Firebase, PrimeNG etc.</li>\\n                            </ul>\\n                        </div>\\n                    </div>\\n                </div>\\n\\n                <div class=\\\"exp__group\\\">\\n                    <div class=\\\"exp__name\\\">\\n                        <img src=\\\"assets/images/crafters.jpeg\\\" alt=\\\"DigitalCrafters\\\">\\n                        <div class=\\\"exp__name__info\\\">\\n                            <h3 class=\\\"title\\\">DigitalCrafters</h3>\\n                            <span class=\\\"years\\\">March 2016 — November 2017</span>\\n                        </div>\\n                    </div>\\n                    <div class=\\\"exp__projects\\\">\\n                        <div class=\\\"exp__projects__item\\\">\\n                            <h3><span class=\\\"md-tag\\\">- [ ]</span> FinTech Project</h3>\\n                            <p>It's an application for IT forums that allows people with big ideas to obtain funding swiftly and easily, even from investors they never had met.</p>\\n                            <ul>\\n                                <li>* <strong>Position</strong>: Junior Front-End Developer</li>\\n                                <li>* <strong>Team</strong>: 10 developers</li>\\n                                <li>* <strong>Technologies</strong>: Angular.js, jQuery, Stylus etc.</li>\\n                            </ul>\\n                        </div>\\n                    </div>\\n                </div>\\n            </div>\\n            <div class=\\\"content-group experience margin-top-40\\\">\\n                <h2><span class=\\\"md-tag\\\">##</span> Education</h2>\\n                <div class=\\\"exp__group margin-top\\\">\\n                    <div class=\\\"exp__name\\\">\\n                        <img src=\\\"assets/images/kpi.jpg\\\" alt=\\\"KPI\\\">\\n                        <div class=\\\"exp__name__info\\\">\\n                            <h3 class=\\\"title\\\">NTUU \\\"Igor Sikorsky Kyiv Polytechnic Institute\\\"</h3>\\n                            <span class=\\\"years\\\">2012 — 2016</span>\\n                        </div>\\n                    </div>\\n                    <div class=\\\"exp__projects\\\">\\n                        <ul>\\n                            <li><span class=\\\"md-tag\\\">- [ ]</span><strong>Major</strong>: Thermal engineering</li>\\n                            <li><span class=\\\"md-tag\\\">- [ ]</span><strong>Specialty</strong>: Computer science</li>\\n                        </ul>\\n                    </div>\\n                </div>\\n            </div>\\n        </div>\\n\\n    </div>\\n</body>\\n</html>\\n\");\n\n//# sourceURL=webpack://romion-cv/./src/index.html?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.html */ \"./src/index.html\");\n/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./scss/app.scss */ \"./src/scss/app.scss\");\n/* harmony import */ var _scss_typography_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./scss/typography.scss */ \"./src/scss/typography.scss\");\n\n\n\n\n\n//# sourceURL=webpack://romion-cv/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	
+/******/ })()
+;
